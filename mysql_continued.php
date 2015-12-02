54195c437f1ba9c2c086d998449ee3e2199dcbf6
ronald.verheul <r.verheul@triple-it.nl>
2015-12-02 16:31:07 +0100

    initial

--- mysql_continued.php
+++ mysql_continued.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,83 +10,109 @@
 
 
 if (!function_exists('mysql_connect')) {
-        $pdo_conn = null;   /*@var $pdo_conn PDO */
-        //$pdo_last = null;   /*@var $pdo_stmt PDOStatement */
-        //$pdo_error = null;  /* the last error message */
-        function mysql_connect($host = 'localhost', $user = '', $pass = '' ) {
-                global $pdo_conn;
-                $pdo_conn = new PDO('sqlite:;host=' . $host, $user, $pass);
+    $pdo_conn = null;       /* @var $pdo_conn PDO */
+    $pdo_error = null;      /* @var $pro_error String */
+    $pdo_last_stmt = null;   /* @var $pdo_stmt PDOStatement */
+
+    define('MYSQL_ASSOC', 1);
+    define('MYSQL_NUM', 2);
+    define('MYSQL_BOTH', 3);
+
+    function mysql_continued() {
+    }
+
+    function mysql_connect($host = 'localhost', $user = '', $pass = '') {
+        global $pdo_conn, $pdo_error;
+        //$pdo_conn = new PDO('sqlite:host=' . $host, $user, $pass);
+        $pdo_conn = new PDO('sqlite:foo.db');
 //                $pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $pdo_conn;
+
+        mysql_store_error($pdo_conn);
+        return $pdo_conn;
+    }
+
+    function mysql_select_db($dbname) {
+        global $pdo_conn, $pdo_error;
+        $rst = (boolean) mysql_query(sprintf("USE %s", mysql_real_escape_string($dbname)));
+        mysql_store_error($rst);
+        return $rst;
+    }
+
+    function mysql_set_charset($charset) {
+        global $pdo_conn, $pdo_error;
+        $rst = (boolean) mysql_query(sprintf('SET NAMES %s', mysql_real_escape_string($charset)));
+        mysql_store_error($rst);
+        return $rst;
+    }
+
+    function mysql_real_escape_string($unescaped_string) {
+        global $pdo_conn;
+        return preg_replace("/'(.*)'/", '$1', $pdo_conn->quote($unescaped_string));
+    }
+
+    function mysql_error() {
+        global $pdo_error;
+        return $pdo_error;
+    }
+
+    function mysql_insert_id() {
+        global $pdo_conn, $pdo_error;
+        $rst = $pdo_conn ? $pdo_conn->lastInsertId() : null;
+        mysql_store_error($rst);
+        return $rst;
+    }
+
+    function mysql_num_rows(PDOStatement $stmt) {
+        global $pdo_conn;
+        $cnt = $pdo_conn->query("SELECT FOUND_ROWS()");
+        return (integer) ($cnt ? $cnt->fetchColumn() : 0);
+    }
+
+    function mysql_affected_rows() {
+        global $pdo_last_stmt;
+        return $pdo_last_stmt == null ? null : $pdo_last_stmt->rowCount();
+    }
+
+    function mysql_query($sql) {
+        global $pdo_conn, $pdo_error, $pdo_last_stmt;
+        $rst = $pdo_conn->query($sql);
+        mysql_store_error($rst);
+        $pdo_last_stmt = $rst;      //store the last stmt for use in affected rows
+        return $rst;
+    }
+
+    function mysql_fetch_assoc($rs) {
+        return $rs->fetch(PDO::FETCH_ASSOC);
+    }
+
+    function mysql_fetch_row($rs) {
+        return $rs->fetch(PDO::FETCH_NUM);
+    }
+
+    function mysql_fetch_array($rs, $result_type = MYSQL_BOTH) {
+        switch ($result_type) {
+            case MYSQL_ASSOC:
+                return mysql_fetch_assoc($rs);
+            case MYSQL_NUM:
+                return mysql_fetch_row($rs);
+            default :
+                return $rs->fetch(PDO::FETCH_BOTH);
         }
-        function mysql_select_db($dbname) {
-                global $pdo_conn;
-                return (boolean) mysql_query(sprintf("USE %s", mysql_real_escape_string($dbname)));
-        }
-        function mysql_set_charset($charset) {
-                global $pdo_conn;
-                return (boolean) mysql_query(sprintf('SET NAMES %s' , mysql_real_escape_string($charset)));
-        }
-        function mysql_real_escape_string($unescaped_string) {
-                global $pdo_conn;
-                return preg_replace("/'(.*)'/", '$1', $pdo_conn->quote($unescaped_string));
-        }
-        function mysql_error() {
-                global $pdo_conn;
-                return $pdo_conn->errorInfo()[2];
-        }
-        function mysql_insert_id() {
-                global $pdo_conn;
-                return $pdo_conn->lastInsertId();
-        }
-        
-        
-        function mysql_query($sql) {
-                global $pdo_conn;
-                return $pdo_conn->query($sql);
-        }
-        function mysql_fetch_assoc($rs) {
-                return $rs->fetch(PDO::FETCH_ASSOC);
-        }
-        // ... etc.
+    }
+
+    /**
+     * PRIVATE
+     * @global PDO $pdo_conn
+     * @global type $pdo_error
+     * @param type $rst
+     */
+    function mysql_store_error($rst) {
+        global $pdo_conn, $pdo_error;
+        $pdo_error = $rst ? null : $pdo_conn->errorInfo()[2];
+    }
+
 }
 
-/*
-//connect
-$dbh = mysql_connect();
-assert($pdo_conn != null);
-assert($pdo_conn === $dbh);
-
-//mysql_error (at handle level)
-assert(mysql_error() == null);
-
-//mysql_select_db
-$bool = mysql_select_db('abc');
-assert($bool === true, "Bool: " . $bool);
-
-//mysql_set_charset
-$bool = mysql_set_charset('utf8');
-assert($bool === true, "Bool: " . $bool);
-
-//mysql_error (at statement level)
-assert(mysql_error() === 'near "SET": syntax error', mysql_error());
-
-//real-escape
-$escaped = mysql_real_escape_string("abc 'stu");
-assert($escaped === "abc ''stu", "value: $escaped");
-
-//verify errors for wrong query
-$rst = mysql_query("select 1 from dual");
-assert(mysql_error() === "no such table: dual");
-
-//mysql_insert_id
-assert(mysql_insert_id() === 1, "Insert id: " . mysql_insert_id());
-
-
-
-
-echo "ok: " . $pdo_conn->getAttribute(constant("PDO::ATTR_SERVER_VERSION")) . "\n";
-*/
 /*
  * To test:
  * 
@@ -94,12 +120,13 @@ echo "ok: " . $pdo_conn->getAttribute(constant("PDO::ATTR_SERVER_VERSION")) . "\
 X mysql_select_db($dbname)
 X mysql_connect()
 X mysql_set_charset('utf8')
-mysql_affected_rows()
-mysql_num_rows($cursor)
-mysql_fetch_array($cursor)
+X mysql_affected_rows()
+X mysql_num_rows($cursor)
+X mysql_fetch_array($cursor)
 X mysql_insert_id()
 X mysql_real_escape_string($http_vars['autidSel'])
 X mysql_error()
-mysql_query($query)
+X mysql_query($query)
+X mysql_fetch_row() 
 
  */
--- test/mysqlContinued.php
+++ test/mysqlContinued.php
@@ -15,14 +15,203 @@ require "../mysql_continued.php";
  * @author ronald
  */
 class mysqlContinued extends PHPUnit_Framework_TestCase {
-    public function testCanConnect() {
-        $dbh = mysql_connect();
-        
-        $this->assertNotNull($dbh);
-        
-        global $pdo_conn;
-        $this->assertEquals($dbh, $pdo_conn);
+    const HOSTNAME = 'localhost';
+    const USERNAME = '';
+    const PASSWORD = '';
+    const DATABASE = 'abc';
+    const TABLENAME = 'v70lgvf2p3b5';
+    
+    private $dbh;
+    private static $pdo;
 
+    public static function setUpBeforeClass(){
+        //self::$pdo = new PDO(sprintf('sqlite:memory:host=%s;dbname=%s;charset=UTF8', self::HOSTNAME, self::DATABASE), 
+        self::$pdo = new PDO('sqlite:foo.db',
+            self::USERNAME, 
+            self::PASSWORD,
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,));
+
+        
+        //create a test table
+        self::$pdo->exec(sprintf("create table %s  (id INTEGER PRIMARY KEY AUTOINCREMENT, col1 string(200));", self::TABLENAME, self::TABLENAME));
+        
     }
+    
+    public static function tearDownAfterClass() {
+        self::$pdo->exec(sprintf("drop table if exists %s;", self::TABLENAME));        
+    }
+    
+    public function setUp() {
+        parent::setUp();
+        $this->dbh = mysql_connect(self::HOSTNAME, self::USERNAME, self::PASSWORD);
+        mysql_select_db(self::DATABASE);
+        mysql_set_charset('utf8');
+    }
+    public function tearDown() {
+        parent::tearDown();
+        $this->clearTable();
+    }
+    
+    public function testLibraryIsLoaded() {
+        $this->assertTrue(function_exists('mysql_continued'));
+    }
+    public function testCanConnect() {
+        global $pdo_conn;
+
+        $this->assertNotNull($this->dbh);
+        $this->assertSame($this->dbh, $pdo_conn);
+        $this->assertNull(mysql_error());
+    }
+    public function testCanSelectDb() {
+        $bool = mysql_select_db(self::DATABASE);
+        $this->assertTrue($bool);        
+        $this_>assertNull(mysql_error());
+    }
+    public function testCanSetCharset() {
+        $bool = mysql_set_charset('utf8');
+        $this->assertTrue($bool);        
+        $this_>assertNull(mysql_error());
+    }
+    public function testRealEscapeData() {
+        $escaped = mysql_real_escape_string("abc 'stu");
+        $this->assertSame("abc ''stu", $escaped);
+    }
+    public function testCanDetectError() {
+        $rst = mysql_query("select 1 from dual");
+        $this->assertFalse($rst);
+        $this->assertSame("no such table: dual", mysql_error());
+
+        $rst = mysql_query("selectx 1 from dual");
+        $this->assertFalse($rst);
+        $this->assertSame('near "selectx": syntax error', mysql_error());
+    }
+    public function testCanInsert() {
+        $rst = mysql_query(sprintf("insert into %s values(null, 'insert')", self::TABLENAME));
+        $this->assertNull(mysql_error());
+        $this->assertTrue((boolean) $rst);
+        $this->assertSame(1, mysql_affected_rows());
+    }
+    public function testCanUpdate() {
+        $this->insertRowsAndSelect(4);
+        $lastId = mysql_insert_id();
+        
+        $rst2 = mysql_query(sprintf("update %s set col1 = 'my Value' where id  = %s", self::TABLENAME, $lastId));
+        $this->assertNull(mysql_error());
+        $this->assertTrue((boolean) $rst2);
+        $this->assertSame(1, mysql_affected_rows());
+
+        $rst3 = mysql_query(sprintf("select * from %s where id  = %s", self::TABLENAME, $lastId));
+//        $this->assertEquals(1, mysql_num_rows($rst3));
+        $row3 = mysql_fetch_assoc($rst3);
+        $this->assertSame('my Value', $row3['col1']);
+    }
+    public function testCanDelete() {
+        $this->insertRowsAndSelect(4);
+        
+        $rst = mysql_query(sprintf("delete from %s", self::TABLENAME));
+        $this->assertNull(mysql_error());
+        $this->assertTrue((boolean) $rst);
+        $this->assertSame(4, mysql_affected_rows());
+    }
+    public function testCanUpdateZeroRows() {
+        $rst = mysql_query(sprintf("update %s set col1 = 'my Value' where 1=0", self::TABLENAME));
+        $this->assertNull(mysql_error());
+        $this->assertTrue((boolean) $rst);
+        $this->assertSame(0, mysql_affected_rows());
+    }
+    public function testCanFetchAssocAndDoWhile() {
+        $rowCnt = 4;
+        $this->insertRowsAndSelect($rowCnt);
+        
+        $rst = mysql_query(sprintf("select * from %s  order by id", self::TABLENAME));
+//        $this->assertEquals($rowCnt, mysql_num_rows($rst));
+        $cnt = 0;
+        while($row = mysql_fetch_assoc($rst)) {
+            $this->assertTrue((integer) $row['id'] > 0);
+            if ($cnt == 0) {
+                $this->assertSame('Row 1', $row['col1']);
+            }
+            $this->assertArrayNotHasKey(0, $row);
+            $this->assertArrayNotHasKey(1, $row);
+            $cnt++;
+        }
+        $this->assertSame($rowCnt, $cnt);
+    }
+    public function testCanFetchArrayAsBoth() {
+        $stmt = $this->insertRowsAndSelect(2);
+        
+        $row = mysql_fetch_array($stmt, MYSQL_BOTH);
+        $this->assertSame($row[0], $row['id']);
+        $this->assertSame('Row 1', $row[1]);
+        $this->assertSame('Row 1', $row['col1']);
+    }
+    public function testCanFetchArrayAsNumArray() {
+        $stmt = $this->insertRowsAndSelect(2);
+        
+        $row = mysql_fetch_array($stmt, MYSQL_NUM);
+        $this->assertSame('Row 1', $row[1]);
+        $this->assertArrayNotHasKey('col1', $row);
+    }
+    public function testCanFetchRow() {
+        $stmt = $this->insertRowsAndSelect(2);
+        
+        $row = mysql_fetch_row($stmt);
+        $this->assertSame('Row 1', $row[1]);
+        $this->assertArrayNotHasKey('col1', $row);
+    }
+    public function testCanReturnLastId() {
+        $this->insertRowsAndSelect();
+
+        $lastId = mysql_insert_id();
+        $this->assertNotNull($lastId);
+        $this->assertTrue($lastId>=1);
+
+        $this->insertRowsAndSelect();
+        $lastId2 = mysql_insert_id();
+        $this->assertTrue($lastId < $lastId2);
+    }
+    
+    public function testCanHandleUtf8() {
+        $testValue = 'Ïnterñátiön€l';
+        
+        $rst = mysql_query(sprintf("insert into %s values(null, '%s')", self::TABLENAME, $testValue));
+        $this->assertNull(mysql_error());
+        $this->assertTrue((boolean) $rst);
+        $this->assertSame(1, mysql_affected_rows());
+        
+        $rst = mysql_query(sprintf("select * from %s order by id", self::TABLENAME));
+        $row = mysql_fetch_assoc($rst);
+        $this->assertSame($testValue, $row['col1']);
+    }
+    
+    
+    /**
+     * @param int $cnt
+     * @return PDOStatement
+     */
+    private function insertRowsAndSelect($cnt = 1) {
+        for ($i = 0; $i < $cnt; $i++) {
+            $rst = mysql_query(sprintf("insert into %s values(null, 'Row %s')", self::TABLENAME, $i+1));
+        }
+        return mysql_query(sprintf("select * from %s", self::TABLENAME));
+    }
+    private function clearTable() {
+        mysql_query(sprintf("delete from %s", self::TABLENAME));
+    }
+    
+    
+ /*TODO: 
+  * enable the asserts for row count
+  * test error conditions:
+    - invalid query
+  * connect failed
+  * select db failed
+  * invalid charset
+  * last insert id not found
+  * num rows emtpy
+  * affected rows empty
+  * 
+  */
+    
 
 }
